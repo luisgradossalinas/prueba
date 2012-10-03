fd0ff94af6664fb6a7708aa1c9f9b91bb2934f55
Martin Grados <martin.programmer@gmail.com>
2012-10-03 14:31:50 -0500

    cambios

--- public/index.php
+++ public/index.php
@@ -1,12 +1,37 @@
 <?php
-/**
- * This makes our life easier when dealing with paths. Everything is relative
- * to the application root now.
- */
-chdir(dirname(__DIR__));
 
-// Setup autoloading
-include 'init_autoloader.php';
+// Define path to application directory
+defined('APPLICATION_PATH')
+    || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/../application'));
 
-// Run the application!
-Zend\Mvc\Application::init(include 'config/application.config.php')->run();
+// Define application environment
+defined('APPLICATION_ENV')
+    || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
+
+// Ensure library/ is on include_path
+set_include_path(implode(PATH_SEPARATOR, array(
+    realpath(APPLICATION_PATH . '/../library'),
+    get_include_path(),
+)));
+
+/** Zend_Application */
+require_once 'Zend/Application.php';
+
+require_once 'Zend/Loader/Autoloader.php';
+Zend_Loader_Autoloader::getInstance ();
+
+$translator = new Zend_Translate(
+                'array',
+                '../resources/languages',
+                'es',
+                array('scan' => Zend_Translate::LOCALE_DIRECTORY)
+);
+Zend_Validate_Abstract::setDefaultTranslator($translator);
+
+// Create application, bootstrap, and run
+$application = new Zend_Application(
+    APPLICATION_ENV,
+    APPLICATION_PATH . '/configs/application.ini'
+);
+$application->bootstrap()
+            ->run();
